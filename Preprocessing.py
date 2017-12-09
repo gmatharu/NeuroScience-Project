1ec988b0fcbb76027bf86c402f947914d2d45536
gmatharu <matharu.gagan@gmail.com>
2017-12-09 18:11:04 +0200

    Code to extract EEG and fMRI Pairs
    Signed-off-by: gmatharu <matharu.gagan@gmail.com>

---
+++ Preprocessing.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import scipy.io
+from nilearn import image
+import numpy as np
+import os
+
+
+def main():
+    #Data Dir
+    data_dir='/gpfs/hpchome/gagand87/project/Data/'
+    os.chdir(data_dir)
+
+    behav_files=list(filter(None,os.popen('find sub0* -name behavdata.txt').read().split('\n')))
+
+    output_df = pd.DataFrame(np.zeros((125,2)))
+    output_df = output_df.astype('object')
+    output_df.columns=['EEG','fMRI']
+
+    # During test run the hpc showed weird error to access list elements by index.
+    # use range [0:1], rather than [0]
+    # For each subject each task each run we find stimulus onsets and then corresponding EEG and fMRI data
+    for file in behav_files:
+        stimulus = pd.read_csv(file,delim_whitespace=True)
+        stimulus_onsets_int = (stimulus['TrialOnset']).astype(int)
+        stimulus_onsets = (stimulus['TrialOnset'] * 1000).astype(int)
+
+        fsplits = file.split('/')
+        #EEG file
+        eeg_file=data_dir+fsplits[0]+'/EEG/'+fsplits[2]+'/EEG_noGA.mat'
+        #fMRI file
+        fmri_file=data_dir + fsplits[0] + '/BOLD/' + fsplits[2] + '/bold_mcf_brain.nii.gz'
+
+        output_file = data_dir+fsplits[0]+'_'+fsplits[2]+'_df.pkl'
+        eeg = scipy.io.loadmat(eeg_file)
+
+        # we move 2 seconds in time if the round down stimulus onset is even, if its odd, we move 3 sec ahead
+        # then we divide by 2 to find the image number, currently we assume first image at 2 sec
+        fMRI_image_indexes = [int((x + 2) / 2) if x % 2 == 0 else int((x + 3) / 2) for x in stimulus_onsets_int]
+
+        for index, fMRI_image_index in enumerate(fMRI_image_indexes):
+            print(index, fMRI_image_index)
+            output_df.iloc[index, 0] = eeg['data_noGA'][:43, stimulus_onsets[index]:stimulus_onsets[index] + 1000]
+            output_df.iloc[index, 1] = image.index_img(fmri_file,fMRI_image_index)
+        #Save the df
+        output_df.to_pickle(output_file )
+
+
+if __name__ == '__main__':
+    main()
